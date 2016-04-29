1007bb84b4a8f117d78c1251ed718e58d7c27ba5
Nikita Dubrovin <nikita.dubrovin@gmail.com>
2016-04-29 21:57:11 +0300

    Revert "bootstrap css integration attempt"
    This reverts commit d6f5dc39e8840ceb2dddbf5db5097dde70d8e30e.

--- client/app/components/order/order.js
+++ client/app/components/order/order.js
@@ -2,10 +2,9 @@ import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import orderComponent from './order.component';
 import postService from './order.service';
-import 'angular-ui-bootstrap';
 
 let orderModule = angular
-        .module('order', [ 'ui.bootstrap', uiRouter ])
+    .module('order', [ uiRouter ])
     .config(($stateProvider) => {
         "ngInject";
         $stateProvider.state('order', {
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,35 @@
 var path    = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
-
-var extractCSS = new ExtractTextWebpackPlugin('[name].css', { allChunks: true });
-var extractStyl = new ExtractTextWebpackPlugin('[name].styl');
 
 module.exports = {
-    devtool: 'sourcemap',
-    context: __dirname + '/client',
-    entry: { app: ['./client/app/app.js'] },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
-            { test: /\.html$/, loader: 'raw' },
-            { test: /\.styl$/, loader: 'style!css!stylus' },
-            { test: /\.css$/, loader: extractCSS.extract('style!css!') }
-        ]
-    },
-    resolve: {
-        modulesDirectories: ['../node_modules'],
-        extensions: ['', '.js', '.css']
-    },
-    plugins: [
-        extractCSS,
-        // Injects bundles in your index.html instead of wiring all manually.
-        // It also adds hash to all injected assets so we don't have problems
-        // with cache purging during deployment.
-        new HtmlWebpackPlugin({
-            template: 'client/index.html',
-            inject: 'body',
-            hash: true
-        }),
-
-        // Automatically move all modules defined outside of application directory to vendor bundle.
-        // If you are using more complicated project structure, consider to specify common chunks manually.
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            minChunks: function (module, count) {
-                return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1;
-            }
-        }),
+  devtool: 'sourcemap',
+  entry: {},
+  module: {
+    loaders: [
+       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
+       { test: /\.html$/, loader: 'raw' },
+       { test: /\.styl$/, loader: 'style!css!stylus' },
+       { test: /\.css$/, loader: 'style!css' }
     ]
+  },
+  plugins: [
+    // Injects bundles in your index.html instead of wiring all manually.
+    // It also adds hash to all injected assets so we don't have problems
+    // with cache purging during deployment.
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+      inject: 'body',
+      hash: true
+    }),
+
+    // Automatically move all modules defined outside of application directory to vendor bundle.
+    // If you are using more complicated project structure, consider to specify common chunks manually.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: function (module, count) {
+        return module.resource && module.resource.indexOf(path.resolve(__dirname, 'client')) === -1;
+      }
+    })
+  ]
 };
